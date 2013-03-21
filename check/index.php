2958848e5fb198cc5ae6c1753305a6b8db78b904
Montana Flynn <montana@montanaflynn.me>
2013-03-20 18:40:43 -0700

    First working example of a spellcheck

---
+++ check/index.php
@@ -0,0 +1,49 @@
+<?php
+
+// Could you tell I have WordPress background?
+// This will probably be changed in the very near future
+
+function spell_word($word) 
+{
+  // The real magic provided by aspell / pspell
+  // http://php.net/manual/en/book.pspell.php
+  global $pspell;
+  
+  $word = $word[0];
+  
+  // ACROYNMS / UPPERCASE
+  if (preg_match('/^[A-Z]*$/',$word))
+  {
+    return $word;
+  } 
+
+  if (pspell_check($pspell,$word)) 
+  {
+    return $word;
+  }
+
+  if ($suggestions = pspell_suggest($pspell,$word)) 
+  {
+    return current($suggestions);
+  }
+
+  return $word;
+}
+
+function spell_check($string) 
+{
+  return preg_replace_callback('/\b\w+\b/','spell_word',$string);
+}
+
+// Finally let's begin the show
+$pspell = pspell_new('en','canadian','','utf-8',PSPELL_FAST);
+
+$text = $_GET['text'];
+
+if ($text) {
+  echo spell_check($text);
+  die();
+}
+
+
+?>
